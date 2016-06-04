389e8996b33619b81281433b560923d78c2a1fc1
adion <andredion@gmail.com>
2016-06-04 14:22:58 -0300

    Adds !rickyism command with initial seed of Rickyisms

--- commands.js
+++ commands.js
@@ -4,5 +4,15 @@ module.exports = {
         process: (bot, msg, args) => {
             bot.sendMessage(msg.channel, msg.sender + ' pong');
         }
+    },
+
+    'rickyism': {
+        description: 'a random bit of knowledge from our pal, Ricky',
+        process: (bot, msg, args) => {
+            let rickyisms = require('./rickyisms.json'),
+                rickyism = rickyisms[Math.floor(Math.random() * rickyisms.length)];
+
+            bot.sendMessage(msg.channel, rickyism);
+        }
     }
 };
